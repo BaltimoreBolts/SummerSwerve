024f61e8b1ca705ed30b5ec29eed6d989d7c883b
irageller1 <87346239+irageller1@users.noreply.github.com>
2023-11-13 21:57:57 -0500

    Test

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -59,9 +59,9 @@ public class Swerve extends SubsystemBase {
       forwardBack = Math.abs(forwardBack) < Constants.kControls.AXIS_DEADZONE ? 0 : forwardBack;
       leftRight = Math.abs(leftRight) < Constants.kControls.AXIS_DEADZONE ? 0 : leftRight;
       rotation = Math.abs(rotation) < Constants.kControls.AXIS_DEADZONE ? 0 : rotation;
-
+      
       // Converting to m/s
-      forwardBack *= Constants.kSwerve.MAX_VELOCITY_METERS_PER_SECOND;
+      forwardBack *= Constants.kSwerve.MAX_VELOCITY_METERS_PER_SECOND; 
       leftRight *= Constants.kSwerve.MAX_VELOCITY_METERS_PER_SECOND;
       rotation *= Constants.kSwerve.MAX_ANGULAR_RADIANS_PER_SECOND;
 
